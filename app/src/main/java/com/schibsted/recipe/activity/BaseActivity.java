1140a9c648c9cffe4b8b586326fa202fd2f6b7d9
samkirton <sam.kirton.uk@gmail.com>
2016-02-28 15:39:42 +0000

    * Added the image to the RecipeDetailsActivity

--- app/src/main/java/com/schibsted/recipe/activity/BaseActivity.java
+++ app/src/main/java/com/schibsted/recipe/activity/BaseActivity.java
@@ -3,6 +3,7 @@ package com.schibsted.recipe.activity;
 import android.app.Activity;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -14,12 +15,6 @@ import rx.Subscriber;
 import rx.functions.Action1;
 
 public abstract class BaseActivity extends AppCompatActivity {
-
-    protected void showHomeUp() {
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setDisplayShowTitleEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
 
     protected void hideSoftKeyboard(View view) {
         InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
--- app/src/main/java/com/schibsted/recipe/activity/RecipeDetailsActivity.java
+++ app/src/main/java/com/schibsted/recipe/activity/RecipeDetailsActivity.java
@@ -1,7 +1,14 @@
 package com.schibsted.recipe.activity;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.graphics.Palette;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -9,6 +16,8 @@ import com.schibsted.recipe.DefaultApplication;
 import com.schibsted.recipe.R;
 import com.schibsted.recipe.bean.Recipe;
 import com.schibsted.recipe.presenter.RecipeDetailPresenter;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Callback;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -16,6 +25,10 @@ import butterknife.ButterKnife;
 public class RecipeDetailsActivity extends BaseActivity implements RecipeDetailsView {
     @Bind(R.id.activity_recipe_details_progressbar) ProgressBar mProgressBar;
     @Bind(R.id.activity_recipe_details_error) TextView mErrorMessageTextView;
+    @Bind(R.id.activity_recipe_details_content) ViewGroup mContentViewGroup;
+    @Bind(R.id.activity_recipe_details_toolbar) Toolbar mToolbar;
+    @Bind(R.id.activity_recipe_details_collapsing_toolbar) CollapsingToolbarLayout mCollapsingToolbarLayout;
+    @Bind(R.id.activity_recipe_details_image) ImageView mRecipeImageView;
 
     private RecipeDetailPresenter mRecipeDetailPresenter;
 
@@ -26,9 +39,13 @@ public class RecipeDetailsActivity extends BaseActivity implements RecipeDetails
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_details);
-        setTitle(getRecipeTitle());
-        showHomeUp();
         ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+        mCollapsingToolbarLayout.setTitle(getRecipeTitle());
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         mRecipeDetailPresenter = new RecipeDetailPresenter(DefaultApplication.getInstance().getApiManager(),this);
     }
@@ -51,21 +68,34 @@ public class RecipeDetailsActivity extends BaseActivity implements RecipeDetails
                 getIntent().getStringExtra(RECIPE_TITLE) : getString(R.string.activity_recipe_details_title_empty);
     }
 
+    private void applyPalette(Palette palette) {
+        int primaryDark = getResources().getColor(R.color.primary_dark);
+        int primary = getResources().getColor(R.color.primary);
+        mCollapsingToolbarLayout.setContentScrimColor(palette.getMutedColor(primary));
+        mCollapsingToolbarLayout.setStatusBarScrimColor(palette.getDarkMutedColor(primaryDark));
+        supportStartPostponedEnterTransition();
+    }
+
     @Override
     public void loadingRecipe() {
         mProgressBar.setVisibility(View.VISIBLE);
         mErrorMessageTextView.setVisibility(View.GONE);
+        mContentViewGroup.setVisibility(View.GONE);
     }
 
     @Override
     public void displayRecipe(Recipe recipe) {
         mProgressBar.setVisibility(View.GONE);
         mErrorMessageTextView.setVisibility(View.GONE);
+        mContentViewGroup.setVisibility(View.VISIBLE);
+
+        Picasso.with(this).load(recipe.getImage_url()).into(mRecipeImageView, mPicassoCallback);
     }
 
     @Override
     public void noRecipeFound() {
         mProgressBar.setVisibility(View.GONE);
+        mContentViewGroup.setVisibility(View.GONE);
         mErrorMessageTextView.setVisibility(View.VISIBLE);
         mErrorMessageTextView.setText(getString(R.string.activity_recipe_details_no_result));
     }
@@ -73,6 +103,7 @@ public class RecipeDetailsActivity extends BaseActivity implements RecipeDetails
     @Override
     public void errorFetchingRecipe() {
         mProgressBar.setVisibility(View.GONE);
+        mContentViewGroup.setVisibility(View.GONE);
         mErrorMessageTextView.setVisibility(View.VISIBLE);
         mErrorMessageTextView.setText(getString(R.string.activity_recipe_details_error));
     }
@@ -81,4 +112,20 @@ public class RecipeDetailsActivity extends BaseActivity implements RecipeDetails
     protected boolean shouldAnimateOut() {
         return true;
     }
+
+    private Callback mPicassoCallback = new Callback() {
+
+        @Override
+        public void onSuccess() {
+            Bitmap bitmap = ((BitmapDrawable) mRecipeImageView.getDrawable()).getBitmap();
+            Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+                public void onGenerated(Palette palette) {
+                    applyPalette(palette);
+                }
+            });
+        }
+
+        @Override
+        public void onError() { }
+    };
 }
